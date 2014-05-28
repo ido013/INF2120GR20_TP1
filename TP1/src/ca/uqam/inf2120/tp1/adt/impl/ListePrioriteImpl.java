f34309b7add818b17f033fdeed4f0b44dfb7555e
Ido013 <tobal_no_1@hotmail.com>
2014-05-27 21:32:21 -0400

    test

---
+++ TP1/src/ca/uqam/inf2120/tp1/adt/impl/ListePrioriteImpl.java
@@ -0,0 +1,131 @@
+package ca.uqam.inf2120.tp1.adt.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import ca.uqam.inf2120.tp1.adt.ListePrioriteTda;
+import ca.uqam.inf2120.tp1.adt.Priorite;
+
+/**
+ * UQAM - Été 2014 - INF2120 - Groupe 20 - TP1
+ * 
+ * ListePrioriteImpl : Implémentation de l'interface ListePrioriteTda.
+ * 
+ * @author Bernatchez, Jonathan et BERJ23118609
+ * @version 27 mai 2014
+ */
+public class ListePrioriteImpl<T extends Priorite> implements ListePrioriteTda<T> {
+
+	private List<T> liste = new ArrayList<T>();
+	
+	// Constructeur
+	public ListePrioriteImpl() {
+		liste = new ArrayList<T>();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#ajouter(ca.uqam.inf2120.tp1.adt.Priorite)
+	 */
+	@Override
+	public boolean ajouter(T elt) {
+		boolean reponse=false;
+		boolean fin=false;
+		int index=0;
+		if(elt!=null && !liste.contains(elt) && !(elt.obtenirPriorite()<=0)){
+			ListIterator<T> it = liste.listIterator();
+			while (it.hasNext() && !fin) {
+				if(it.next().obtenirPriorite()==elt.obtenirPriorite()){
+					
+				}
+			}
+		}
+		
+		return reponse;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#ajouter(java.util.List)
+	 */
+	@Override
+	public void ajouter(List<T> liste) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#supprimer(ca.uqam.inf2120.tp1.adt.Priorite)
+	 */
+	@Override
+	public boolean supprimer(T elt) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#supprimer(java.util.List)
+	 */
+	@Override
+	public List<T> supprimer(List<T> liste) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#supprimer(int)
+	 */
+	@Override
+	public List<T> supprimer(int priorite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#supprimer(int, boolean)
+	 */
+	@Override
+	public boolean supprimer(int priorite, boolean plusPetit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#remplacer(int, int)
+	 */
+	@Override
+	public boolean remplacer(int anciennePriorite, int nouvellePriorite) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#ObtenirNbElments(int)
+	 */
+	@Override
+	public int ObtenirNbElments(int priorite) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * Vérifie si la liste existante est vide.
+	 * 
+	 * @return Vrai si la liste est vide, sinon faux
+	 */
+	@Override
+	public boolean estVide() {
+		return liste.isEmpty();
+	}
+
+	/* (non-Javadoc)
+	 * @see ca.uqam.inf2120.tp1.adt.ListePrioriteTda#iterateur()
+	 */
+	@Override
+	public ListIterator<T> iterateur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
---
+++ TP1/src/ca/uqam/inf2120/tp1/adt/test/ListePrioriteImplTest.java
@@ -0,0 +1,146 @@
+package ca.uqam.inf2120.tp1.adt.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.uqam.inf2120.tp1.adt.ListePrioriteTda;
+import ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl;
+
+/**
+ * UQAM - Été 2014 - INF2120 - Groupe 20 - TP1
+ * 
+ * ListePrioriteImplTest : Classe de test de ListePrioriteImpl.
+ * 
+ * @author Bernatchez, Jonathan et BERJ23118609
+ * @version 27 mai 2014
+ */
+public class ListePrioriteImplTest {
+	
+	ListePrioriteTda<ElementT> listePriorite;
+	ElementT elt1, elt2, elt3, elt4, elt5, elt6, elt7, elt8;
+	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		listePriorite = new ListePrioriteImpl<ElementT>();
+		elt1 = new ElementT("elt1",1);
+		elt2 = new ElementT("elt2",2);
+		elt3 = new ElementT("elt3",3);
+		elt4 = new ElementT("elt4",4);
+		elt5 = new ElementT("elt5",5);
+		elt6 = new ElementT("elt6",6);
+		elt7 = new ElementT("elt7",1);
+		elt8 = new ElementT("elt8",2);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		listePriorite = null;
+		elt1 = null;
+		elt2 = null;
+		elt3 = null;
+		elt4 = null;
+		elt5 = null;
+		elt6 = null;
+		elt7 = null;
+		elt8 = null;
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#ListePrioriteImpl()}.
+	 */
+	@Test
+	public void testListePrioriteImpl() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#ajouter(ca.uqam.inf2120.tp1.adt.Priorite)}.
+	 */
+	@Test
+	public void testAjouterT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#ajouter(java.util.List)}.
+	 */
+	@Test
+	public void testAjouterListOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#supprimer(ca.uqam.inf2120.tp1.adt.Priorite)}.
+	 */
+	@Test
+	public void testSupprimerT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#supprimer(java.util.List)}.
+	 */
+	@Test
+	public void testSupprimerListOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#supprimer(int)}.
+	 */
+	@Test
+	public void testSupprimerInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#supprimer(int, boolean)}.
+	 */
+	@Test
+	public void testSupprimerIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#remplacer(int, int)}.
+	 */
+	@Test
+	public void testRemplacer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#ObtenirNbElments(int)}.
+	 */
+	@Test
+	public void testObtenirNbElments() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#estVide()}.
+	 */
+	@Test
+	public void testEstVide() {
+		assertTrue(listePriorite.estVide());
+	}
+
+	/**
+	 * Test method for {@link ca.uqam.inf2120.tp1.adt.impl.ListePrioriteImpl#iterateur()}.
+	 */
+	@Test
+	public void testIterateur() {
+		fail("Not yet implemented");
+	}
+
+}
